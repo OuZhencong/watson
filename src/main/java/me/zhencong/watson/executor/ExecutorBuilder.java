44864a90289cd48882d7e7283bfe82c1b36cdde4
ouzhencong <ouzhencong@outlook.com>
2015-07-13 00:14:06 +0800

    建立基本的代码结构。

---
+++ src/main/java/me/zhencong/watson/executor/ExecutorBuilder.java
@@ -0,0 +1,14 @@
+package me.zhencong.watson.executor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author <a href="ozhencong@gmail.com">Ou.Zhencong</a>
+ * @since 15/7/13
+ */
+public class ExecutorBuilder {
+
+    public Executor build() {
+        return null;
+    }
+}
